4282058b5a2ad7930dc79871db8f512f3ba2c726
jkr78 <jurdanas.kriauciunas@gmail.com>
2018-03-25 21:40:17 +0200

    Fix speed

--- mgserv/mgserv.go
+++ mgserv/mgserv.go
@@ -16,7 +16,7 @@ func main() {
 	port := flag.Int("port", 1201, "port")
 	width := flag.Int("width", mg.BoardWidth, "board width")
 	height := flag.Int("height", mg.BoardWidth, "board height")
-	speed := flag.Int("speed", -mg.LoopSpin, "speed")
+	speed := flag.Int("speed", mg.LoopSpin, "speed in secs")
 	crlf := flag.Bool("crlf", false, "use CRLF as end of line (default LF)")
 
 	flag.Parse()
@@ -37,7 +37,7 @@ func main() {
 		sep = mg.LF[:]
 	}
 
-	game := mg.NewGame(*width, *height, -*speed)
+	game := mg.NewGame(*width, *height, *speed)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
